babe7b6ba443448b6e01edd6e8cbb5911a899352
habiba.khalid <habiba.khalid@linkdev.com>
2016-11-02 15:29:47 +0200

    First push

---
+++ app/src/main/java/com/eventtus/task/view/ActivityLogin.java
@@ -0,0 +1,15 @@
+package com.eventtus.task.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eventtus.task.R;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
